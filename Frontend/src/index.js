2a0016ededcec79a7c8096b79fe7ac6a6c911efb
AVGfx <avgfx15@gmail.com>
2025-01-30 06:50:51 +0530

    Check in local app is running but on deploy server not connected

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'; // Use `react-dom/client`
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+
 import store from './store';
 import { Provider } from 'react-redux';
 
